df7bf09f61ba1a6b153b68f0e1e8bbc9c9d9f27b
eddym <eddyats11"gmail.com>
2021-07-01 16:03:11 +0300

    updates

--- converting_to_pdf.py
+++ converting_to_pdf.py
@@ -121,6 +121,130 @@ def create_pdf(n):
                 pdf.cell(page_width, 0.0, '- end of report -', align='C')
 
                 pdf.output('school.pdf', 'F')
+
+    if n == 4:
+        trail.students_table()
+        with open('pdfs/students_table.csv', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
+                pdf = FPDF()
+                pdf.add_page()
+                page_width = pdf.w - 2 * pdf.l_margin
+
+                pdf.set_font('Times', 'B', 14.0)
+                pdf.cell(page_width, 0.0, 'Students Data', align='C')
+                pdf.ln(10)
+
+                pdf.set_font('Courier', '', 8)
+
+                col_width = page_width / 8
+
+                pdf.ln(1)
+
+                th = pdf.font_size
+
+                for row in reader:
+                    # print(row)
+                    pdf.cell(col_width, th, str(row[0]), border=1)
+                    pdf.cell(col_width, th, row[1], border=1)
+                    pdf.cell(col_width, th, row[2], border=1)
+                    pdf.cell(col_width, th, row[3], border=1)
+                    pdf.cell(col_width, th, row[4], border=1)
+                    pdf.cell(col_width, th, row[5], border=1)
+                    pdf.cell(col_width, th, row[6], border=1)
+                    pdf.cell(col_width, th, row[7], border=1)
+                    pdf.ln(th)
+
+                pdf.ln(10)
+
+                pdf.set_font('Times', '', 10.0)
+                pdf.cell(page_width, 0.0, '- end of report -', align='C')
+
+                pdf.output('student.pdf', 'F')
+
+        trail.teachers_table()
+        with open('pdfs/teacher_table.csv', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
+                pdf = FPDF()
+                pdf.add_page()
+                page_width = pdf.w - 2 * pdf.l_margin
+
+                pdf.set_font('Times', 'B', 14.0)
+                pdf.cell(page_width, 0.0, 'Teachers Data', align='C')
+                pdf.ln(10)
+
+                pdf.set_font('Courier', '', 12)
+
+                col_width = page_width / 5
+
+                pdf.ln(1)
+
+                th = pdf.font_size
+
+                for row in reader:
+                    # print(row)
+                    pdf.cell(col_width, th, str(row[0]), border=1)
+                    pdf.cell(col_width, th, row[1], border=1)
+                    pdf.cell(col_width, th, row[2], border=1)
+                    pdf.cell(col_width, th, row[3], border=1)
+                    pdf.cell(col_width, th, row[4], border=1)
+                    pdf.ln(th)
+
+                pdf.ln(10)
+
+                pdf.set_font('Times', '', 10.0)
+                pdf.cell(page_width, 0.0, '- end of report -', align='C')
+
+                pdf.output('teachers.pdf', 'F')
+
+        trail.schools_table()
+        with open('pdfs/school_table.csv', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(row)
+                pdf = FPDF()
+                pdf.add_page()
+                page_width = pdf.w - (-2) * pdf.l_margin
+
+                pdf.set_font('Times', 'B', 14.0)
+                pdf.cell(page_width, 0.0, 'Schools Data', align='C')
+                pdf.ln(10)
+
+                pdf.set_font('Courier', '', 8)
+
+                col_width = page_width / 8
+
+                pdf.ln(1)
+
+                th = pdf.font_size
+
+                for row in reader:
+                    # print(row)
+                    pdf.cell(col_width, th, str(row[0]), border=1)
+                    pdf.cell(col_width, th, row[1], border=1)
+                    pdf.cell(col_width, th, row[2], border=1)
+                    pdf.cell(col_width, th, row[3], border=1)
+                    pdf.cell(col_width, th, row[4], border=1)
+                    pdf.cell(col_width, th, row[5], border=1)
+                    pdf.cell(col_width, th, row[6], border=1)
+                    pdf.ln(th)
+
+                pdf.ln(10)
+
+                pdf.set_font('Times', '', 10.0)
+                pdf.cell(page_width, 0.0, '- end of report -', align='C')
+
+                pdf.output('school.pdf', 'F')
+
+
+
+
+
+
+
 def read_pdf(n):
     if n==1:
         # creating a pdf file object
--- send_email.py
+++ send_email.py
@@ -1,4 +1,61 @@
+import os
+import smtplib
+import trail
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('email')
+EMAIL_PASSWORD = os.environ.get('password')
+
+def number_of_contact(n,meso,cont,pdf):
+    content=cont
+    msg = EmailMessage()
+    meso=meso
+    contacts = n
+    msg['Subject'] = meso
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = contacts
+    msg.set_content(content)
+
+    files=pdf
+    for file in files:
+        with open(file, 'rb') as f:
+            file_data = f.read()
+            file_name= f.name
+        msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+
+    smtp= smtplib.SMTP_SSL( 'smtp.gmail.com', 465)
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
 
 
 
-tghjm
+
+
+
+def main(att):
+        meso=input("Enter the subject:\n")
+        content=input("Enter message to receiver\n")
+
+        pdf=[]
+        all=['student.pdf','teachers.pdf','school.pdf']
+        if att ==1:
+            pdf.append('student.pdf')
+        if att ==2:
+            pdf.append('teachers.pdf')
+        if att == 3:
+            pdf.append('school.pdf')
+        if att == 4:
+            pdf= all
+
+        em= int(input("enter the number of emails to send:  "))
+        n=[]
+        for i in range(em):
+            emails=input("enter email; \n")
+            n.append(emails)
+        print("these are the emails to be sent to ", n)
+        number_of_contact(n, meso, content, pdf)
+
+if __name__ == '__main__':
+    main()
